31bea04b916ff71f22d075ae53d8d8ec82b5f7a5
suatii06 <w0508798@nscc.ca>
2026-01-29 10:01:12 -0400

    initial commit

---
+++ People.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Lab2
+{
+    class People : INotifyPropertyChanged {
+        private ObservableCollection<Person> _people;
+        public ObservableCollection<Person> PeopleList {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public People() {
+            _people = new ObservableCollection<Person>() {
+                new Person{ 
+                    ID = 1, 
+                    FirstName = "John", 
+                    LastName = "Doe", 
+                    Salary = 50000, 
+                    Color = Color.LightGray, 
+                    Address = "123 Main St", 
+                    Birthday = new DateOnly(1990, 1, 1), 
+                    Email = "john.doe@example.com", 
+                    Notes = "No notes", 
+                    PhoneNumber = "555-1234" },
+                new Person{ 
+                    ID = 2, 
+                    FirstName = "Jane", 
+                    LastName = "Smith", 
+                    Salary = 60000, 
+                    Color = Color.LightGray, 
+                    Address = "456 Elm St", 
+                    Birthday = new DateOnly(1992, 2, 2), 
+                    Email = "jane.smith@example.com", 
+                    Notes = "No notes", 
+                    PhoneNumber = "555-5678" },
+                new Person{ 
+                    ID = 3, 
+                    FirstName = "Michael", 
+                    LastName = "Johnson", 
+                    Salary = 55000, 
+                    Color = Color.LightGray, 
+                    Address = "789 Oak St", 
+                    Birthday = new DateOnly(1995, 3, 3), 
+                    Email = "michael.johnson@example.com", 
+                    Notes = "No notes", 
+                    PhoneNumber = "555-8765" },
+            };
+        }
+
+        public void AddPerson(Person person) {
+            _people.Add(person);
+        }
+
+        public void RemovePerson(Person person) {
+            _people.Remove(person);
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2 {
+    class Person {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private int _salary;
+        private string _phoneNumber;
+        private string _email;
+        private string _address;
+        private DateOnly _birthday;
+        private Color _color;
+        private String _notes;
+
+        public int ID {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string FirstName {
+            get { return _firstName; }
+            set {
+                if(this._firstName != value)
+                    _firstName = value;
+            }
+        }
+        public string LastName {
+            get { return _lastName; }
+            set {
+                if(this._lastName != value)
+                    _lastName = value;
+            }
+        }
+        public int Salary {
+            get { return _salary; }
+            set {
+                if(this._salary != value)
+                    _salary = value;
+            }
+        }
+        public string PhoneNumber {
+            get { return _phoneNumber; }
+            set {
+                if(this._phoneNumber != value)
+                    _phoneNumber = value;
+            }
+        }
+        public string Email {
+            get { return _email; }
+            set {
+                if(this._email != value)
+                    _email = value;
+            }
+        }
+        public string Address {
+            get { return _address; }
+            set {
+                if(this._address != value)
+                    _address = value;
+            }
+        }
+        public DateOnly Birthday {
+            get { return _birthday; }
+            set {
+                if(this._birthday != value)
+                    _birthday = value;
+            }
+        }
+        public Color Color {
+            get { return _color; }
+            set {
+                if(this._color != value)
+                    _color = value;
+            }
+        }
+        public String Notes {
+            get { return _notes; }
+            set {
+                if(this._notes != value)
+                    _notes = value;
+            }
+        }
+
+        public Person() {
+            this._id = 0;
+            this._firstName = string.Empty;
+            this._lastName = string.Empty;
+            this._salary = 0;
+            this._phoneNumber = string.Empty;
+            this._email = string.Empty;
+            this._address = string.Empty;
+            this._birthday = DateOnly.MinValue;
+            this._color = Color.LightGray;
+            this._notes = string.Empty;
+        }
+    }
+}
